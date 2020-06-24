4cb0f954f85e9c986d7df1f42f11864eba82d6cf
Brads <bradleyjsnider@gmail.com>
2020-06-24 09:16:49 -0700

    Removed references to ProjectService to get app running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EsriMapComponent } from './esri-map/esri-map.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
-import {IdentityManagementService} from './services/identity-management.service';
+import { IdentityManagementService } from './services/identity-management.service';
 import { MapViewComponent } from './map-view/map-view.component';
 import { ListViewComponent } from './list-view/list-view.component';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {LoadingService} from './services/loading.service';
-import {DataService} from './services/data.service';
-import {ProjectService} from './services/project.service';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { LoadingService } from './services/loading.service';
+import { DataService } from './services/data.service';
+// import { ProjectService } from './services/project.service';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -37,7 +37,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatPaginatorModule,
     BrowserAnimationsModule
   ],
-  providers: [IdentityManagementService, LoadingService, DataService, ProjectService],
+  providers: [IdentityManagementService,
+    LoadingService,
+    DataService,
+    // ProjectService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
