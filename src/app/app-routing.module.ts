6c86d8d47e99bdb99576cf29708d0e48ce94fff2
cfourrou <cfourroux@innovateteam.com>
2020-06-30 15:14:35 -0700

    Update CDA specific info.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,26 +24,30 @@
 // export class AppRoutingModule { }
 
 
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MapViewComponent } from './map-view/map-view.component';
-import { IdentityManagementService } from './services/identity-management.service';
-import { ListViewComponent } from './list-view/list-view.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { InvoiceComponent } from './invoice/invoice.component';
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MapViewComponent} from './map-view/map-view.component';
+import {IdentityManagementService} from './services/identity-management.service';
+import {ListViewComponent} from './list-view/list-view.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {InvoiceComponent} from './invoice/invoice.component';
 
 
 const routes: Routes = [
   {
-    path: 'app', component: MapViewComponent, canActivate: [IdentityManagementService], children: [
-      { path: 'projects', component: ListViewComponent },
-      { path: '', redirectTo: 'projects', pathMatch: 'full' }
+    path: 'SnoqualmieEN', children: [
+      {
+        path: 'app', component: MapViewComponent, canActivate: [IdentityManagementService], children: [
+          {path: 'projects', component: ListViewComponent},
+          {path: '', redirectTo: 'projects', pathMatch: 'full'}
+        ]
+      },
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'invoice', component: InvoiceComponent, pathMatch: 'full'},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'}
     ]
-  },
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'invoice', component: InvoiceComponent, pathMatch: 'full' },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
+  }
+
 ];
 
 @NgModule({
