d594e2080b0ab09c7da6ee9fd41015fa47a85f19
cfourrou <cfourroux@innovateteam.com>
2020-07-08 20:45:21 -0700

    Add a snoq environment file

---
+++ src/environments/environment.snoq.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  jsapiConfig: {url: 'https://js.arcgis.com/4.15/'},
+  maxAttachments: 4,
+  name: 'Assert',
+  portalSetting : {
+    url: 'https://maps.snoqualmietribe.us/portal',
+    appId: 'diCIRzmGi5yW8nVJ',
+    mapId_1: '839f096da1a94ebb8ec5323a2d166193',
+    mapId_2: 'c838538b432943508f2a7fd568b0a161',
+    mapId_3: '01d331569c8b4127829d21eac99d20b4',
+    mapId: '63b1c93bfe8e42dca58459f47db6ebe0'
+  },
+  restSetting : {
+    url: 'https://arcserver.innovateteam.com/arcgis/rest/services/TCRIS/TCRIS/FeatureServer/',
+    report: 'https://arcserver.innovateteam.com/arcgis/rest/services/TCRIS/'
+  }
+};
+
