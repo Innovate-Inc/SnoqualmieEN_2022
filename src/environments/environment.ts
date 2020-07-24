d0d3f61c831f2e6a293a9b927a8802b30028085a
cfourrou <cfourroux@innovateteam.com>
2020-07-24 09:08:23 -0700

    Updated example map

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
     appId: '5d2rgNDIEPIHgjwj',
     mapId_1: '839f096da1a94ebb8ec5323a2d166193',
     mapId_2: 'c838538b432943508f2a7fd568b0a161',
-    mapId: '63b1c93bfe8e42dca58459f47db6ebe0'
+    mapId_3: '63b1c93bfe8e42dca58459f47db6ebe0',
+    mapId: '01d331569c8b4127829d21eac99d20b4'
   },
   layers: {
     review: `${url}/0`,
