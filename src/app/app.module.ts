6b13396c1fbb56fe15aa2822d7a2faf4e07173ff
Dustin Pierce <dustin@piercese.com>
2020-10-14 14:35:04 -0500

    Added changes notification and editing on/off feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 import { MapViewComponent } from './map-view/map-view.component';
@@ -28,25 +28,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { WildCardComponent } from './wild-card/wild-card.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import {ProjectService} from './services/project.service';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ProjectService } from './services/project.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { EditPaneComponent } from './edit-pane/edit-pane.component';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { ReviewFormComponent } from './review-form/review-form.component';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AbstractFormComponent } from './abstract-form/abstract-form.component';
 import { SupportDocsFormComponent } from './support-docs-form/support-docs-form.component';
 import { ImpactsFormComponent } from './impacts-form/impacts-form.component';
 import { ActivityFormComponent } from './activity-form/activity-form.component';
-import {DocuService} from './services/docu.service';
+import { DocuService } from './services/docu.service';
 import { DocPopupComponent } from './doc-popup/doc-popup.component';
 import { MatRadioModule } from '@angular/material/radio';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
 import { CommentFormComponent } from './activity-sub-modules/comment-form/comment-form.component';
 import { HearingFormComponent } from './activity-sub-modules/hearing-form/hearing-form.component';
 import { MeetingFormComponent } from './activity-sub-modules/meeting-form/meeting-form.component';
@@ -61,6 +61,7 @@ import { MeetingTabComponent } from './activity-sub-modules/meeting-tab/meeting-
 import { HearingTabComponent } from './activity-sub-modules/hearing-tab/hearing-tab.component';
 import { ChanceEncounterTabComponent } from './activity-sub-modules/chance-encounter-tab/chance-encounter-tab.component';
 import { SiteVisitTabComponent } from './activity-sub-modules/site-visit-tab/site-visit-tab.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -90,7 +91,7 @@ import { SiteVisitTabComponent } from './activity-sub-modules/site-visit-tab/sit
     MeetingTabComponent,
     HearingTabComponent,
     ChanceEncounterTabComponent,
-    SiteVisitTabComponent
+    SiteVisitTabComponent,
   ],
   imports: [
     BrowserModule,
@@ -118,6 +119,7 @@ import { SiteVisitTabComponent } from './activity-sub-modules/site-visit-tab/sit
     MatNativeDateModule,
     MatRadioModule,
     MatSliderModule,
+    MatSlideToggleModule,
   ],
   providers: [IdentityManagementService,
     LoadingService,
