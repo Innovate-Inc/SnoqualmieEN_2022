325b0a2520d2cbb24b69a72f7bcb0293022dd2cf
Dustin <dustinmehrapierce@gmail.com>
2020-11-08 19:08:37 -0500

    major updates to activity tab tabs, activity tab tab UI updates, activity loading, pagation, call tab and call form communication

--- src/app/activity-sub-modules/call-tab/call-tab.component.ts
+++ src/app/activity-sub-modules/call-tab/call-tab.component.ts
@@ -1,27 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap, finalize } from 'rxjs/operators';
+import { ArcBaseService } from 'src/app/services/arc-base.service';
+import { LoadingService } from 'src/app/services/loading.service';
+import { environment } from 'src/environments/environment';
 import { CallFormComponent } from '../call-form/call-form.component';
-export interface PeriodicElement {
-  attributes: {
-    Call_Who: string;
-    Call_Type: number;
-    Call_Email: string;
-    Call_Number: number;
-  }
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {attributes: { Call_Type: 1, Call_Who: 'Hydrogen', Call_Number: 1.0079, Call_Email: 'H'}},
-  {attributes:{Call_Type: 2, Call_Who: 'Helium', Call_Number: 4.0026, Call_Email: 'He'}},
-  {attributes:{Call_Type: 3, Call_Who: 'Lithium', Call_Number: 6.941, Call_Email: 'Li'}},
-  {attributes:{Call_Type: 4, Call_Who: 'Beryllium', Call_Number: 9.0122, Call_Email: 'Be'}},
-  {attributes:{Call_Type: 5, Call_Who: 'Boron', Call_Number: 10.811, Call_Email: 'B'}},
-  {attributes:{Call_Type: 6, Call_Who: 'Carbon', Call_Number: 12.0107, Call_Email: 'C'}},
-  {attributes:{Call_Type: 7, Call_Who: 'Nitrogen', Call_Number: 14.0067, Call_Email: 'N'}},
-  {attributes:{Call_Type: 8, Call_Who: 'Oxygen', Call_Number: 15.9994, Call_Email: 'O'}},
-  {attributes:{Call_Type: 9, Call_Who: 'Fluorine', Call_Number: 18.9984, Call_Email: 'F'}},
-  {attributes:{Call_Type: 10, Call_Who: 'Neon', Call_Number: 20.1797, Call_Email: 'Ne'}},
-];
 
 @Component({
   selector: 'app-call-tab',
@@ -30,18 +17,38 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class CallTabComponent implements OnInit {
-  displayedColumns: string[] = ['nature', 'name', 'email', 'number'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Call_Who', "Call_Number", 'CreationDate', 'Creator'];
+  activityService: ArcBaseService;
+  projectId: string;  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
-  constructor(public dialog: MatDialog) { }
-
-  ngOnInit(): void {
+  constructor(public  loadingService: LoadingService, private route: ActivatedRoute, snackBar: MatSnackBar, public dialog: MatDialog) {
+    this.activityService = new ArcBaseService(environment.layers.call,  snackBar, loadingService);
   }
-  openDialog(): void {
-    let dialogRef = this.dialog.open(CallFormComponent, {
+ 
+  ngOnInit(): void {
+    this.loadingService.show();
+    this.route.parent.parent.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        this.projectId = params.get('id');
+        return this.activityService.layerIsLoaded.pipe(
+          switchMap(() => {
+            this.activityService.filter.where = `parentglobalid = '${this.projectId}'`;
+            return this.activityService.getItems().pipe(finalize(() => {
+              this.loadingService.hide();
+            }));
+          })
+        );
+      })).subscribe(()=>this.loadingService.hide());
+  }
+  openDialog(task: any): void {
+    this.dialog.open(CallFormComponent, {
       height: '700px',
       width: '600px',
+      data: {activityTask: task, meta: this.activityService.meta},
+    }).afterClosed().subscribe(confirmed => {
+      this.ngOnInit();
     });
   }
 }
--- src/app/activity-sub-modules/comment-form/comment-form.component.ts
+++ src/app/activity-sub-modules/comment-form/comment-form.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Graphic from 'esri/Graphic';
 import { ArcBaseService } from 'src/app/services/arc-base.service';
 import { DialogService } from 'src/app/services/dialog.service';
+import { LoadingService } from 'src/app/services/loading.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-comment-form',
@@ -9,14 +14,89 @@ import { DialogService } from 'src/app/services/dialog.service';
   styleUrls: ['./comment-form.component.css']
 })
 export class CommentFormComponent implements OnInit {
-
   uploadService: ArcBaseService;
+  activityService: ArcBaseService;
   dialogService: DialogService;
-  
-  constructor(public dialog: MatDialog) {
+  meta: any;
+  objectID: number;
+  isNew: boolean = true;
+
+  activityForm = new FormGroup({
+    Activity_Type: new FormControl(),
+    Activity_Department: new FormControl(),
+    Activity_Staff: new FormControl(),
+    Activity_Date: new FormControl(),
+    Call_Who: new FormControl(),
+    Call_Number: new FormControl(),
+    Call_Email: new FormControl(),
+    Call_Address: new FormControl(),
+    Call_Type: new FormControl(),
+    Call_Doc_Note: new FormControl(),
+    parentglobalid: new FormControl(),
+    CreationDate: new FormControl(),
+    Creator: new FormControl({ value: '', disabled: true }),
+    EditDate: new FormControl(),
+    Editor: new FormControl(),
+    globalid: new FormControl(),
+    objectid: new FormControl()
+  });
+  constructor(public dialog: MatDialog, public snackBar: MatSnackBar, 
+    public loadingService: LoadingService, @Inject(MAT_DIALOG_DATA) public data: any) {
     this.dialogService = new DialogService(this.uploadService, this.dialog)
-   }
-  ngOnInit(): void {
+    this.activityService = new ArcBaseService(environment.layers.call, this.snackBar, this.loadingService);
+    this.dialogService = new DialogService(this.activityService, this.dialog);
+ 
   }
 
+  ngOnInit(): void {
+    this.meta = this.data.meta;
+    this.activityForm.patchValue(this.data.activityTask.attributes);
+    if(this.data.activityTask.attributes.globalid != "new"){
+      console.log(this.data.activityTask);
+      this.isNew = false;
+      this.dialogService.item = this.data.activityTask;
+      this.loadingService.show();
+      this.objectID = this.data.activityTask.attributes.objectid;
+      this.dialogService.getAttachments();
+      this.loadingService.hide()
+    }
+  }
+
+  save(): void {
+
+    if(this.activityForm.valid){
+
+      if (this.isNew) {
+
+        this.data.activityTask.attributes.Activity_Type = "call";
+        this.data.activityTask.attributes.Activity_Date = new Date();
+        //this.data.activityTask.attributes = this.activityForm.value;
+
+        this.data.activityTask.attributes.Call_Who = this.activityForm.controls['Call_Who'].value;
+        this.data.activityTask.attributes.Call_Number = this.activityForm.controls['Call_Number'].value;
+        this.data.activityTask.attributes.Call_Email = this.activityForm.controls['Call_Email'].value;
+        this.data.activityTask.attributes.Call_Address = this.activityForm.controls['Call_Address'].value;
+        this.data.activityTask.attributes.Call_Type = this.activityForm.controls['Call_Type'].value;
+        this.data.activityTask.attributes.Call_Doc_Note = this.activityForm.controls['Call_Doc_Note'].value;
+
+        let feature = new Graphic(this.data.activityTask);
+        this.activityService.addFeature(feature).subscribe((res: Array<any>) =>{
+          this.dialogService.item = new Graphic(res[0]);
+          this.dialogService.item.attributes = {};
+          this.dialogService.item.attributes.objectid = res[0].objectId;
+          this.dialogService.item.attributes.globalid = res[0].globalId;
+          this.data.activityTask.attributes.objectid = res[0].objectId;
+          this.data.activityTask.attributes.globalid = res[0].globalId;
+          this.data.activityTask = new Graphic(this.data.activityTask.attributes);
+          console.log(this.data.activityTask);
+        });
+        this.isNew = false;
+      }  
+      else {
+        this.data.activityTask.attributes = this.activityForm.value;
+        this.activityService.updateFeature(this.data.activityTask).subscribe();
+      }   
+
+    }
+  }   
 }
