3a8161653df081d21d88824145259eff6105f807
Dustin <dustinmehrapierce@gmail.com>
2020-11-22 23:33:39 -0500

    Added filter side menu for searching within a date range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {IdentityManagementService} from './services/identity-management.service'
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectService } from './services/project.service';
 import { DataService } from './services/data.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import moment from 'moment';
 
 
 @Component({
@@ -14,6 +16,10 @@ export class AppComponent {
   title = 'Assert';
   showFiller = false;
 
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+  });
 
    // constructor(private activatedRoute: ActivatedRoute, private identityManager: IdentityManagementService) {
     constructor(public router: Router, private identityManager: IdentityManagementService, 
@@ -25,8 +31,33 @@ export class AppComponent {
     this.identityManager.logout();
     this.router.navigate(['welcome']);
   }
-
-  log(){
-    console.log(this.projectService.editing);
+  applyFilter(num: number){
+    if(num === 0){ // apply filters
+      let fromDate = moment(this.filterForm.controls['fromDate'].value).format("YYYY/MM/DD");
+      let toDate = moment(this.filterForm.controls['toDate'].value).format("YYYY/MM/DD");
+      if(this.filterForm.controls['fromDate'].value && this.filterForm.controls["toDate"].value){
+        this.projectService.filter.where = `created_date between '${fromDate}' and '${toDate}'`;
+      }
+      else if (this.filterForm.controls['fromDate'].value) {
+        this.projectService.filter.where = ` created_date > '${fromDate}'`;
+      }
+      else if(this.filterForm.controls["toDate"].value){
+        this.projectService.filter.where = `created_date < '${toDate}'`;
+      }
+      else {
+        this.projectService.filter.where = `Project_Name like '%'`;
+      }
+      this.projectService.layerIsLoaded.subscribe(() =>{
+        this.projectService.filter.orderByFields = [`ID_DAHP_full DESC`];
+        this.projectService.getItems().subscribe();
+      });
+    }
+    else { //clear all filters
+      this.filterForm.setValue({"fromDate": "", "toDate": ""})
+      this.projectService.layerIsLoaded.subscribe(() => {
+        this.projectService.filter.where = `Project_Name like '%'`;
+        this.projectService.getItems().subscribe();
+      });
+    }
   }
 }
