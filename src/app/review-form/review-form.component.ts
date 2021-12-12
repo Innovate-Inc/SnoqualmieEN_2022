2a5154957263ed63d378c2c33d81f225519f6aa6
= <olindyla@gmail.com>
2021-12-12 18:17:58 -0500

    clear notification tribe text when not selected

--- src/app/review-form/review-form.component.ts
+++ src/app/review-form/review-form.component.ts
@@ -1,31 +1,31 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Project, ProjectService} from '../services/project.service';
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Project, ProjectService } from "../services/project.service";
 // import {EditService} from '../services/edit.service';
-import {finalize, map, switchMap, takeWhile, tap} from 'rxjs/operators';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import { finalize, map, switchMap, takeWhile, tap } from "rxjs/operators";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 // import {MainMapService} from '../services/main-map.service';
 // import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 // import {Observable} from 'rxjs/Observable';
 // import {Subject} from 'rxjs/Subject';
 // import 'rxjs/add/observable/if';
 // import 'rxjs/add/observable/forkJoin';
-import {LoadingService} from '../services/loading.service';
-import {BehaviorSubject, iif, Observable, of, Subject} from 'rxjs';
+import { LoadingService } from "../services/loading.service";
+import { BehaviorSubject, iif, Observable, of, Subject } from "rxjs";
 // import {ProjectActionsLU, ProjectActionsLuService} from '../services/project-actions-lu.service';
 // import {LandOwnershipLU, LandOwnershipLuService} from '../services/land-ownership-lu.service';
 
 @Component({
-  selector: 'app-review-form',
-  templateUrl: './review-form.component.html',
-  styleUrls: ['./review-form.component.css']
+  selector: "app-review-form",
+  templateUrl: "./review-form.component.html",
+  styleUrls: ["./review-form.component.css"],
 })
 export class ReviewFormComponent implements OnInit, OnDestroy {
   review: Subject<any>;
   editObs: any;
   // actions: Subject<ProjectActionsLU> = new Subject<ProjectActionsLU>();
   // owner: Subject<LandOwnershipLU> = new Subject<LandOwnershipLU>();
-  mapMode = 'edit';
+  mapMode = "edit";
   featureForm = new FormGroup({
     // ProjectNumber: new FormControl(),
     OBJECTID: new FormControl(),
@@ -67,12 +67,15 @@ export class ReviewFormComponent implements OnInit, OnDestroy {
     CreationDate: new FormControl(),
     Creator: new FormControl(),
     EditDate: new FormControl(),
-    Editor: new FormControl()
+    Editor: new FormControl(),
   });
 
-  constructor(public projectService: ProjectService, private route: ActivatedRoute, private router: Router,
-              public  loadingService: LoadingService) {
-  }
+  constructor(
+    public projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public loadingService: LoadingService
+  ) {}
 
   //             public mapService: MainMapService, public editService: EditService, public projectActionsLuService: ProjectActionsLuService,
   //             public landOwnershipLuService: LandOwnershipLuService) {
@@ -82,34 +85,41 @@ export class ReviewFormComponent implements OnInit, OnDestroy {
     this.loadingService.show();
     this.review = new BehaviorSubject(null);
     this.loadingService.show();
-    this.route.parent.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        return iif(() => params.get('id') === 'new',
-          this.projectService.create_new_project(),
-          this.projectService.selectFeature(params.get('id'), null));
-      }),
-      map(review => {
-        this.review.next(review);
-        if (review.attributes.globalid !== 'new') {
-          review.attributes.Noti_Dept_Select = review.attributes.Noti_Dept_Select.split(',');
-          this.featureForm.patchValue(review.attributes);
-        }
-      }),
-    ).subscribe(() => this.loadingService.hide());
+    this.route.parent.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => {
+          return iif(
+            () => params.get("id") === "new",
+            this.projectService.create_new_project(),
+            this.projectService.selectFeature(params.get("id"), null)
+          );
+        }),
+        map((review) => {
+          this.review.next(review);
+          if (review.attributes.globalid !== "new") {
+            review.attributes.Noti_Dept_Select =
+              review.attributes.Noti_Dept_Select.split(",");
+            this.featureForm.patchValue(review.attributes);
+          }
+        })
+      )
+      .subscribe(() => this.loadingService.hide());
   }
-
 
   ngOnDestroy() {
     //  this.editObs.unsubscribe();
-    console.log('ngOnDestroy: Aothing to destry yet');
+    console.log("ngOnDestroy: Aothing to destry yet");
     // this.mapService.clearGraphics();
   }
 
   save(review: any) {
     Object.keys(this.featureForm.controls).forEach((key) => {
       this.featureForm.get(key).markAsPristine();
-    });    
+    });
     review.attributes = this.featureForm.value;
+    if (this.featureForm.controls.Noti_Tribe.value != "yes") {
+      review.attributes.Noti_Tribe_Text == "";
+    }
     const reviewDept = review.attributes.Noti_Dept_Select;
     delete review.attributes.Noti_Dept_Select;
     // let land_ownership = review.attributes.Land_Ownership;
@@ -119,8 +129,9 @@ export class ReviewFormComponent implements OnInit, OnDestroy {
     }
     this.projectService.layerIsLoaded.subscribe(() => {
       if (review.attributes.ID_DAHP_full === null) {
-        this.projectService.queryMax().subscribe(projectMaxResults => {
-          review.attributes.ID_DAHP_full = Number(projectMaxResults[0].attributes.ProjectNumMAX) + 1;
+        this.projectService.queryMax().subscribe((projectMaxResults) => {
+          review.attributes.ID_DAHP_full =
+            Number(projectMaxResults[0].attributes.ProjectNumMAX) + 1;
         });
       }
       // Observable.if(() => review.attributes.GlobalID === 'new',
@@ -132,40 +143,39 @@ export class ReviewFormComponent implements OnInit, OnDestroy {
       //       return results;
       //     })
       //   ),
-      this.projectService.updateFeature(review)
-        .subscribe(
-          //   results => {
-          //   // delete all related multiselect values
-          //   this.projectActionsLuService.layerIsLoaded.subscribe(() => {
-          //     this.projectActionsLuService.filter.where = `projectsid_fk = '${results[0].globalId}'`;
-          //     this.projectActionsLuService.query().subscribe(projectActions_results => {
-          //       projectActions_results.forEach((element) => {
-          //         this.projectActionsLuService.delete(element).subscribe();
-          //       });
-          //       if (project_actions instanceof Array) {
-          //         project_actions.forEach((element) => {
-          //           this.projectActionsLuService.create_new_action(results[0].globalId, element).subscribe();
-          //         });
-          //       }
-          //     });
-          //   });
-          //   this.landOwnershipLuService.layerIsLoaded.subscribe(() => {
-          //     this.landOwnershipLuService.filter.where = `project_fk = '${results[0].globalId}'`;
-          //     this.landOwnershipLuService.query().subscribe(landOwner_results => {
-          //       landOwner_results.forEach((element) => {
-          //         this.landOwnershipLuService.delete(element).subscribe();
-          //       });
-          //       if (land_ownership instanceof Array) {
-          //         land_ownership.forEach((element) => {
-          //           this.landOwnershipLuService.create_new_action(results[0].globalId, element).subscribe();
-          //         });
-          //       }
-          //     });
-          //   });
-          // }
-        );
+      this.projectService
+        .updateFeature(review)
+        .subscribe
+        //   results => {
+        //   // delete all related multiselect values
+        //   this.projectActionsLuService.layerIsLoaded.subscribe(() => {
+        //     this.projectActionsLuService.filter.where = `projectsid_fk = '${results[0].globalId}'`;
+        //     this.projectActionsLuService.query().subscribe(projectActions_results => {
+        //       projectActions_results.forEach((element) => {
+        //         this.projectActionsLuService.delete(element).subscribe();
+        //       });
+        //       if (project_actions instanceof Array) {
+        //         project_actions.forEach((element) => {
+        //           this.projectActionsLuService.create_new_action(results[0].globalId, element).subscribe();
+        //         });
+        //       }
+        //     });
+        //   });
+        //   this.landOwnershipLuService.layerIsLoaded.subscribe(() => {
+        //     this.landOwnershipLuService.filter.where = `project_fk = '${results[0].globalId}'`;
+        //     this.landOwnershipLuService.query().subscribe(landOwner_results => {
+        //       landOwner_results.forEach((element) => {
+        //         this.landOwnershipLuService.delete(element).subscribe();
+        //       });
+        //       if (land_ownership instanceof Array) {
+        //         land_ownership.forEach((element) => {
+        //           this.landOwnershipLuService.create_new_action(results[0].globalId, element).subscribe();
+        //         });
+        //       }
+        //     });
+        //   });
+        // }
+        ();
     });
   }
 }
-
-
